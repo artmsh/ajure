eee35016652ae4b8fe5db48854af7e81288dc203
Artem <artm.ua@gmail.com>
2018-01-25 22:53:30 +0200

    CircleCI initial configuration

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [kixi/aleph "0.4.4-alpha5"]
+                 [aleph "0.4.4"]
                  [cheshire "5.8.0"]
                  [expound "0.3.1"]
                  [clj-aql "0.1.1-SNAPSHOT"]])

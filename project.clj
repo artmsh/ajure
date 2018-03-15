eb8ade01c518528cb6a4e2d72e3aa448e8499dc2
Artem <artm.ua@gmail.com>
2018-03-15 15:04:20 +0200

    Version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ajure "0.1.2"
+(defproject ajure "0.1.2-SNAPSHOT"
   :description "Clojure driver for ArangoDB"
   :url "https://github.com/artmsh/ajure"
   :license {:name "Eclipse Public License"
